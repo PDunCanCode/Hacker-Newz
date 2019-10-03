badbbff4887630b0aa231b13d4fb394f58df4f73
Paul D. Duncan <chesiredarling@gmail.com>
2019-10-03 15:05:12 -0700

    added getStory

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
 import React from 'react';
+import { StoriesContainer } from './containers/StoriesContainer';
 
-export const App = () => <p> yo. yo.</p>
+export const App = () => <StoriesContainer />;
